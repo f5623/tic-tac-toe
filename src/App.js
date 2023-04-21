5ce69f5e580eff5f7da16232ed63865142217fc4
arv <aerofaesk@proton.me>
2023-04-21 14:03:26 -0600

    player choose and switch turn

--- src/App.js
+++ src/App.js
@@ -18,6 +18,16 @@ export default function App() {
   }
   randomStartingPlayer() 
 
+  function switchTurn() {
+    if (turns<8) {
+    if (currentPlayer === 'X') {
+      currentPlayer = 'O';
+    } else {
+      currentPlayer = 'X';
+    }
+    turns++;}
+  }
+
 
 /// handle
   function handleClickStart(){
@@ -30,21 +40,51 @@ export default function App() {
       window.location.reload();
 
 }
+function handleClick(event) {
+  const button = event.target;
+  const buttonId = button.id;
+    const clickedButton = event.target;
+    console.log(event.target, 'before change inside event listener ')
+    clickedButton.textContent = currentPlayer;
+ 
+  if (buttonId <= 3) {
+    let col = buttonId-1 
+    gameBoard[0][col]=currentPlayer
+  } else if (buttonId >= 4 && buttonId <= 6 ){
+    let col = buttonId-4 
+    gameBoard[1][col]=currentPlayer
+  } else if (buttonId >= 7 && buttonId <=9){
+    let col = buttonId-7
+    gameBoard[2][col]=currentPlayer
+  } else {
+    console.log('ooops')
+  }
+if (turns<8){
+  switchTurn()
+} else {
+  let x = checkWinner()
+  console.log('winner is:', x)
+}
 
+}
+const buttons = document.querySelectorAll('.square');
+buttons.forEach(button => {
+  button.addEventListener('click', handleClick);
+});
 
   return (
     <div className="container">
         <button className="buttonStart" onClick={handleClickStart}> Start New Game </button>
         <div className="board">
-            <button className="square" id="1" >   </button>
-            <button className="square" id="2" >   </button>
-            <button className="square" id="3" >   </button>
-            <button className="square" id="4" >   </button>
-            <button className="square" id="5" >   </button>
-            <button className="square" id="6" >   </button>
-            <button className="square" id="7" >   </button>
-            <button className="square" id="8" >   </button>
-            <button className="square" id="9" >   </button>
+            <button className="square" id="1" onClick={handleClick}>   </button>
+            <button className="square" id="2" onClick={handleClick}>   </button>
+            <button className="square" id="3" onClick={handleClick}>   </button>
+            <button className="square" id="4" onClick={handleClick}>   </button>
+            <button className="square" id="5" onClick={handleClick}>   </button>
+            <button className="square" id="6" onClick={handleClick}>   </button>
+            <button className="square" id="7" onClick={handleClick}>   </button>
+            <button className="square" id="8" onClick={handleClick}>   </button>
+            <button className="square" id="9" onClick={handleClick}>   </button>
         </div>
     </div> 
   );
